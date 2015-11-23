2c431e71ea63022a0c964ed190bdcffcc33e1dd3
Ian Darwin <ian@darwinsys.com>
2015-11-23 13:46:32 -0500

    Add Hints entity.

---
+++ src/main/java/com/darwinsys/todo/model/Hint.java
@@ -0,0 +1,40 @@
+package com.darwinsys.todo.model;
+
+import javax.persistence.*;
+
+/**
+ * A Hint is one little note to help people improve their use of
+ * ToDo list management.
+ */
+
+@Entity
+public class Hint {
+	long id;
+	String hint;
+	String author;
+
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getHint() {
+		return hint;
+	}
+
+	public void setHint(String hint) {
+		this.hint = hint;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+}
