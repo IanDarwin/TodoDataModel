f347108df5596f47de62ea5348892d9bb852b8fe
Ian Darwin <ian@darwinsys.com>
2021-12-18 19:30:17 -0500

    Replace boring I/O method with a Stream.

--- src/main/java/com/darwinsys/todo/converters/Txt2ToodleDo.java
+++ src/main/java/com/darwinsys/todo/converters/Txt2ToodleDo.java
@@ -1,35 +1,20 @@
 package com.darwinsys.todo.converters;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import com.darwinsys.todo.model.Import;
 import com.darwinsys.todo.model.Task;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Txt2ToodleDo {
 
 	public static void main(String[] args) throws Exception {
-		List<Task> tasks = Import.importTasks(fileToList("/home/ian/TODO.txt"));
+		final String fileName = "/home/ian/TODO.txt";
+		List<Task> tasks =
+				Files.lines(Path.of(fileName)).map(s->Import.importTask(s)).collect(Collectors.toList());
 		final List<String> exportedTasks = new ExportText().export(tasks);
-		for (String s : exportedTasks) {
-			System.out.println(s);
-		}
-	}
-
-	public static List<String> fileToList(String fileName) throws IOException {
-		List<String> ret = new ArrayList<String>(100);
-		BufferedReader is = new BufferedReader(new FileReader(fileName));
-		String line;
-		try {
-			while ((line = is.readLine()) != null) {
-				ret.add(line);
-			}
-		} finally {
-			is.close();
-		}
-		return ret;
+		exportedTasks.forEach(System.out::println);
 	}
 }
